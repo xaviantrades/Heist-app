630cba5ad7e5d193cadeb4364f868b4d4b31887b
xaviantrades <geraldsaviour623@gmail.com>
2026-09-11 17:48:38 +0300

    update login.js

--- login.js
+++ login.js
@@ -31,7 +31,7 @@ function initFormValidation() {
 
   form.addEventListener('submit', (e) => {
     const username = document.getElementById('id_username');
-    const username = document.getElementById('loginbtn');
+    const logolbtn = document.getElementById('loginbtn');
     const password = document.getElementById('id_password');
     let valid = true;
 
